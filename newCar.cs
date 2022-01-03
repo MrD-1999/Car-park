f0e128396e5e05748d9c0bdf9f3b807a1c2bf3e3
MrD-1999 <97032789+MrD-1999@users.noreply.github.com>
2022-01-04 00:54:04 +0200

    Add files via upload

---
+++ newCar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public enum colors { red, blue, yelow }
+    public class newCar
+    {
+       public string Compeny { get; set; }
+       public int CarId { get; set; }
+       public DateTime entryTime { get; set; }
+       public colors color { get; set; } 
+      public string getData()
+        {
+        return $"compeny:{Compeny}\ncarID:{CarId}\nentry time:{entryTime}\ncolor:{color}";
+        }
+    }
+
